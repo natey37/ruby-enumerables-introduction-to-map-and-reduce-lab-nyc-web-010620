b8a2a458a2b1926d818051be84911f5c913fe260
Nathan Brickett <natey37@gmail.com>
2019-12-19 04:51:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,7 @@ def map_to_square(source_array)
   return new_array
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = nil)
   total = 0 
   index = 0 
   while index < source_array.count do 
